f33c0b6d805893fbe6780143fd530f738a0035eb
sumiga.borna <sumiga.borna@gmail.com>
2020-12-21 10:28:52 +0100

    Refactored MockClient to utils

--- test/repository/weather_api_test.dart
+++ test/repository/weather_api_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 
-class MockClient extends Mock implements http.Client {}
+import '../common_test/utils.dart';
 
 void main() {
   /* Written with a help of: https://flutter.dev/docs/cookbook/testing/unit/mocking */
