a2f24cc0157ac3489d4befaf04d7ef1297d57550
sumiga.borna <sumiga.borna@gmail.com>
2020-12-21 14:37:30 +0100

    Refactored tests so they are more reliable (removed changing the json response)

--- test/bloc/weather_bloc_test.dart
+++ test/bloc/weather_bloc_test.dart
@@ -1,29 +1,19 @@
-import 'dart:convert';
-
 import 'package:bloc_practic_app/blocs/weather_bloc.dart';
 import 'package:bloc_practic_app/blocs/weather_events.dart';
 import 'package:bloc_practic_app/blocs/weather_state.dart';
-import 'package:bloc_practic_app/models/weather_data.dart';
 import 'package:bloc_practic_app/repository/weather_repository.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:http/http.dart' as http;
-
-import '../common_test/utils.dart';
 
 class MockWeatherRepository extends Mock implements WeatherRepository {}
 
 void main() {
   group('Weather bloc tests', () {
     WeatherRepository weatherRepository;
-    WeatherBloc weatherBloc;
-    http.Client client;
 
     setUp(() {
       weatherRepository = MockWeatherRepository();
-      weatherBloc = WeatherBloc(weatherRepository: weatherRepository);
-      client = MockClient();
     });
 
     test(
@@ -33,21 +23,13 @@ void main() {
     });
 
     group('Tests to event: getWeather', () {
-      var _successResponse = http.Response(MOCK_OPEN_WEATHER_RESPONSE, 200);
-      var weatherData = WeatherData(
-          jsonDecode(_successResponse.body)['weather'][0]['description']);
       blocTest(
-          //BEFORE YOU RUN THE TEST, IN "MOCK_OPEN_WEATHER_RESPONSE" CHANGE WEATHER DESCRIPTION ("description") TO THE ONE THAT WAS LAST DISPLAYED ON THE DEVICE, OTHERWISE TEST WILL NOT PASS
           'emits [WeatherLoadInProgress, WeatherLoadSuccess], When getWeather is added and succeeds',
-          build: () {
-            when(weatherRepository.getWeather(client))
-                .thenAnswer((_) => Future.value(weatherData));
-            return weatherBloc;
-          },
+          build: () => WeatherBloc(weatherRepository: weatherRepository),
           act: (WeatherBloc bloc) => bloc.add(WeatherEvent.getWeather),
           expect: [
-            WeatherLoadInProgress(),
-            WeatherLoadSuccess(weatherData: weatherData),
+            isA<WeatherLoadInProgress>(),
+            isA<WeatherLoadSuccess>(),
           ]);
     });
   });
