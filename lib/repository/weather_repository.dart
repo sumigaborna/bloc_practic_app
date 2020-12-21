a69bb6601ab577cca9ac1df684a45975b6021ba0
sumiga.borna <sumiga.borna@gmail.com>
2020-12-21 10:29:37 +0100

    Refactored WeatherRepository to have a client variable

--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -2,7 +2,13 @@ import 'package:bloc_practic_app/repository/weather_api.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherRepository {
-  final WeatherApi weatherApi = WeatherApi();
+  WeatherRepository([this._client]);
 
-  Future getWeather() async => await weatherApi.getWeather(new http.Client());
+  http.Client _client;
+
+  final WeatherApi _weatherApi = WeatherApi();
+
+  //client can be null because it's a optional value, hence if it's null new client will be created
+  Future getWeather() async =>
+      await _weatherApi.getWeather(_client ?? new http.Client());
 }
