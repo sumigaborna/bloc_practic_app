3303384a70513e865c384d5c8393a0d2cc87fe0b
sumiga.borna <sumiga.borna@gmail.com>
2020-12-21 13:29:29 +0100

    Weather Bloc tests written and passing

--- test/bloc/weather_bloc_test.dart
+++ test/bloc/weather_bloc_test.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
 import 'package:bloc_practic_app/blocs/weather_bloc.dart';
 import 'package:bloc_practic_app/blocs/weather_events.dart';
+import 'package:bloc_practic_app/blocs/weather_state.dart';
 import 'package:bloc_practic_app/models/weather_data.dart';
 import 'package:bloc_practic_app/repository/weather_repository.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+
+import '../common_test/utils.dart';
 
 class MockWeatherRepository extends Mock implements WeatherRepository {}
 
@@ -25,16 +31,20 @@ void main() {
     });
 
     group('Tests to event: getWeather', () {
+      var _successResponse = http.Response(MOCK_OPEN_WEATHER_RESPONSE, 200);
+      var weatherData = WeatherData(
+          jsonDecode(_successResponse.body)['weather'][0]['description']);
       blocTest(
-          'emits [WeatherInitial, WeatherLoadInProgress, WeatherLoadSuccess], When getWeather is added and succeds',
+          'emits [WeatherInitial, WeatherLoadInProgress, WeatherLoadSuccess], When getWeather is added and succeeds',
           build: () {
-            when(weatherRepository.getWeather()).thenAnswer((realInvocation) =>
-                Future.value(WeatherData('Oh look a test weather')));
+            when(weatherRepository.getWeather())
+                .thenAnswer((_) => Future.value(weatherData));
             return weatherBloc;
           },
           act: (WeatherBloc bloc) => bloc.add(WeatherEvent.getWeather),
           expect: [
-            weatherBloc.
+            WeatherLoadInProgress(),
+            WeatherLoadSuccess(weatherData: weatherData),
           ]);
     });
   });
